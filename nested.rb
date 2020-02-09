ce7c4e6b7ec9ed63affa5188e387ae391e85f4a7
Harmandeep Mand <harmandeepmand.hm@gmail.com>
2020-02-09 04:37:29 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -39,7 +39,7 @@ def alan_kay_is_known_for
         }
      }
      
-  values = programmer_hash[:alan_kay]
+  values = programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -58,6 +58,9 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     
+  languages = programmer_hash[:dennis_ritchie][:languages]
+  value = languages[0]
 end
 
 def adding_matz
